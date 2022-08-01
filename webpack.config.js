8a5b2a883b64a240695f90fb22c96657fff9d87c
Angel Pantoja Mayorga <angel.pantoja@distillery.com>
2022-08-01 14:41:57 -0500

    curso react

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const env = process.env.NODE_ENV || 'development'
+module.exports = {
+  mode: env,
+  output: {
+    filename: 'app.bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+        }
+      }
+    ]
+  }
+}
