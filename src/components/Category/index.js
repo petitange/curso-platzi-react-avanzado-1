2e3e5b86995fd8d7adc8369058efad11e03cdb26
Angel Pantoja <angelpan17@hotmail.com>
2022-08-15 11:51:54 -0500

    Classes 1-13

---
+++ src/components/Category/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Anchor, Image } from './styles'
+
+const DEFAULT_IMAGE = 'https://static.platzi.com/media/user_upload/styled-comp-609781f2-82ec-476e-9250-579ee8b016fb.jpg'
+
+export const Category = ({ cover = DEFAULT_IMAGE, path, emoji = '?'}) => (
+  <Anchor href={path}>
+    <Image src={cover} />
+    {emoji}
+  </Anchor>
+)
